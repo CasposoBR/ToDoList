4a7769e4d2ab803ab923e6a65e458b94b65a67f7
Ryan Guilherme <ryanguilherme.1708@hotmail.com>
2025-02-24 22:27:43 -0300

    Adicionado a classe TasksPreferences, Adicionado a função de caso o usuario sair do app e voltar, as informações continuarem. Imcompleto neste commit pois não está salvando o que o usuario marcou confirmado

---
+++ app/src/main/java/TasksPreferences.kt
@@ -0,0 +1,10 @@
+import android.content.Context
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class TasksPreference( ){
+
+
+
+}
